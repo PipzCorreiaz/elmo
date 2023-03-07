ced987c3047d9150a7bafebb892d896003805e9f
Filipa Correia <filipaincorreia@gmail.com>
2023-03-07 12:33:55 +0000

    added script for synchrony behavior

---
+++ catkin_elmo/src/elmo/src/synchrony_behavior.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python3
+
+
+import random
+import rospy
+import robot as r
+from std_msgs.msg import String
+
+
+MIN_SLEEP = 2.0
+MAX_SLEEP = 4.0
+
+
+class Node:
+
+    def __init__(self):
+        self.pan_tilt_api = r.PanTilt()
+        self.server_api = r.Server()
+        self.onboard_api = r.Onboard()
+        self.enabled = False
+        rospy.set_param("behaviour/synchrony_behaviour/enabled", self.enabled)
+        rospy.Subscriber("behaviour/enable", String, self.on_enable)
+        rospy.Subscriber("behaviour/disable", String, self.on_disable)
+
+    def on_enable(self, msg):
+        if msg.data != "synchrony_behaviour" or self.enabled:
+            return
+        print("enabling test motors")
+        self.pan_tilt_api.enable(True, True)
+        self.pan_tilt_api.reset_angles()
+        self.enabled = True
+        rospy.set_param("behaviour/synchrony_behaviour/enabled", self.enabled)
+
+    def on_disable(self, msg):
+        if msg.data != "synchrony_behaviour" or not self.enabled:
+            return
+        print("disabling test motors")
+        self.enabled = False
+        self.pan_tilt_api.reset_angles()
+        rospy.sleep(2.0)
+        self.pan_tilt_api.enable(False, False)
+        rospy.set_param("behaviour/synchrony_behaviour/enabled", self.enabled)
+
+    def run(self):
+        rate = rospy.Rate(10)
+        url = self.server_api.url_for_image("normal.png")
+        self.onboard_api.set_image(url)
+        i = 0
+        self.enabled = True
+        pan_array = [80, 80, 80, 80]
+        tilt_array = [0.1, 15, -15, 15]
+        sleep_array = [5.0, 5.0, 2.0, 2.0]
+        while i < 4:
+            rate.sleep()
+            if self.enabled:
+                limits = self.pan_tilt_api.get_limits()
+                pan = pan_array[i]
+                tilt = tilt_array[i]
+                self.pan_tilt_api.set_angles(
+                    pan=pan,
+                    tilt=tilt
+                )
+                rospy.loginfo("pan: %.2f | tilt: %.2f" % (pan, tilt))
+                rospy.sleep(sleep_array[i])
+            i += 1
+        self.enabled = False
+        
+
+
+if __name__ == '__main__':
+    rospy.init_node("synchrony_behaviour")
+    NODE = Node()
+    rospy.loginfo("synchrony_behaviour: running")
+    NODE.run()
